bfaefaf02857c3d867dea41e2856a40a4b44de17
Husain Mulyansyah <145633025+HusainD4@users.noreply.github.com>
2024-05-16 16:52:34 +0700

    update

---
+++ dictionary.py
@@ -0,0 +1,12 @@
+person = {'nama' : 'Noah', 'age' : '33', 'city' : 'tegal'}
+
+person['fav'] = 'bakso'
+print(person)
+
+person['age']  = '17'
+print(person)
+
+del person ['city']
+print(person)
+
+print(person['fav'])
